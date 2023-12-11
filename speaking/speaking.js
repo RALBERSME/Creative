0327bde9fe33b392397ce67df5c9f19d518f19d7
RALBERSME <149826808+RALBERSME@users.noreply.github.com>
2023-12-11 21:55:32 +0100

    update of message
    update of message

--- speaking/speaking.js
+++ speaking/speaking.js
@@ -4,12 +4,12 @@ btn.addEventListener("click", func);
 function func() {
   const mytext = document.getElementById("text").value;
   const secondText =
-    "It could sound better. You can learn to become a great speaker. This project is unfortunately still under construction....It will be updated in the next few months. See you then again.";
+    "It could sound better. You can learn to become a great speaker.Therefore continue the course by pressing the black button below. You will find a lot of material, tips and tricks to improve your speaking skills. Good luck.";
   var msg = new SpeechSynthesisUtterance();
   msg.text = mytext + secondText;
   window.speechSynthesis.speak(msg);
 
   alert(
-    "Oh my dear friend, - wasn`t that horrible? We will show you how to become a better speaker...at the moment, this project is unfortunately still under construction ....It will be updated in the next few months. See you then again. For now you might enjoy the tongue twisters. Thanks."
+    "Oh my dear friend, - wasn`t that horrible? We will show you how to become a better speaker...Therefore continue the course by pressing the black button below. You will find a lot of material, tips and tricks to improve your speaking skills. Good luck."
   );
 }
