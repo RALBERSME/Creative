dcd34a12e3305a9cac326971552806be58a8e47f
RALBERSME <149826808+RALBERSME@users.noreply.github.com>
2023-12-15 09:39:46 +0100

    spoken message changed

--- speaking/speaking.js
+++ speaking/speaking.js
@@ -4,12 +4,12 @@ btn.addEventListener("click", func);
 function func() {
   const mytext = document.getElementById("text").value;
   const secondText =
-    "It could sound better. You can learn to become a great speaker.Therefore continue the course by pressing the black button below. You will find a lot of material, tips and tricks to improve your speaking skills. Good luck.";
+    "This was just spoken by a computer, but you can do far better. You can learn to become a great speaker.Therefore continue the course by pressing the black button below. You will find a lot of material, tips and tricks to improve your speaking skills. Good luck.";
   var msg = new SpeechSynthesisUtterance();
   msg.text = mytext + secondText;
   window.speechSynthesis.speak(msg);
 
-  alert(
+  /* alert(
     "Oh my dear friend, - wasn`t that horrible? We will show you how to become a better speaker...Therefore continue the course by pressing the black button below. You will find a lot of material, tips and tricks to improve your speaking skills. Good luck."
-  );
+  )*/
 }
